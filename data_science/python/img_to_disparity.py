dfa1937a377b381c9700b996c5c597443bc2ba69
David Evans <evansdav.a@gmail.com>
2020-02-15 15:45:08 -0800

    Added checks to debug image property variance

--- data_science/python/img_to_disparity.py
+++ data_science/python/img_to_disparity.py
@@ -1,5 +1,6 @@
 import cv2
 from matplotlib import pyplot as plt
+from numpy import ndim
 
 
 def img_to_disp(img_tup):
@@ -10,13 +11,17 @@ def img_to_disp(img_tup):
     y = 125
     imgL_trim = imgL[y:y + h].copy()
     imgR_trim = imgR[y:y + h].copy()
+
+    if imgL.ndim == 3:
+        imgL_trim = cv2.cvtColor(imgL_trim, cv2.COLOR_BGR2GRAY)
+        imgR_trim = cv2.cvtColor(imgR_trim, cv2.COLOR_BGR2GRAY)
     disparity = stereo.compute(imgL_trim, imgR_trim)
     return disparity
 
 
 if __name__ == '__main__':
-    imgL = cv2.imread('data_science/data/tsukuba_l.png', 0)
-    imgR = cv2.imread('data_science/data/tsukuba_r.png', 0)
+    imgL = cv2.imread('../data/tsukuba_l.png', 0)
+    imgR = cv2.imread('../data/tsukuba_r.png', 0)
     disp = img_to_disp((imgL, imgR))
     plt.imshow(disp, 'gray')
     plt.show()
