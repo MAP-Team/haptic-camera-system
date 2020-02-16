9ad814ec80a97c76115d830c8c325e13bf99f808
Tanner York <tannerwyork@gmail.com>
2020-02-15 16:01:28 -0800

    Added code to connect depthmap to haptic calculations

--- data_science/python/main.py
+++ data_science/python/main.py
@@ -1,5 +1,6 @@
 from stream_object import Stream
 from img_to_disparity import img_to_disp as i2d
+from depth_to_haptic import depth_to_haptic
 # from matplotlib import pyplot as plt
 
 cam1 = Stream()
@@ -8,4 +9,5 @@ cam1.run()
 while cam1.size() > 0:
     tup = cam1.dequeue()
     disp = i2d(tup)
-    print(disp)
+    haptic = depth_to_haptic(disp)
+    print(haptic)
