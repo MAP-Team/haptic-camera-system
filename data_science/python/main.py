c661852fd3ac83fe135774a42fabc565b5567942
Kevin <kevinm1776@gmail.com>
2020-02-16 15:53:40 -0800

    Added plt.show

--- data_science/python/main.py
+++ data_science/python/main.py
@@ -1,7 +1,7 @@
 from stream_object import Stream
 from img_to_disparity import img_to_disp as i2d
 from depth_to_haptic import depth_to_haptic
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 
 cam1 = Stream()
 cam1.run()
@@ -10,4 +10,4 @@ while cam1.size() > 0:
     tup = cam1.dequeue()
     disp = i2d(tup)
     haptic = depth_to_haptic(disp, 62.2)
-    print(haptic)
+    plt.imshow(haptic)
