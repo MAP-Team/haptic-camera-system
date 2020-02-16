336a6900447ad04c5a80ac84fc40e0b68987ce74
David Evans <evansdav.a@gmail.com>
2020-02-15 16:30:32 -0800

    Added comments to img_to_disparity

--- data_science/python/img_to_disparity.py
+++ data_science/python/img_to_disparity.py
@@ -5,21 +5,29 @@ from numpy import ndim
 
 def img_to_disp(img_tup):
     """Converts a tuple of images to an opencv disparity"""
+    # Creating objects to manipulate
     stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
     imgL, imgR = img_tup
+
+    # Trimming images to 16px tall, starting at y=125px
     h = 16
     y = 125
     imgL_trim = imgL[y:y + h].copy()
     imgR_trim = imgR[y:y + h].copy()
 
+    # Check if image is grayscale or not
     if imgL.ndim == 3:
+        # Convert it if not
         imgL_trim = cv2.cvtColor(imgL_trim, cv2.COLOR_BGR2GRAY)
         imgR_trim = cv2.cvtColor(imgR_trim, cv2.COLOR_BGR2GRAY)
+    
+    # Compute and return disparity values
     disparity = stereo.compute(imgL_trim, imgR_trim)
     return disparity
 
 
 if __name__ == '__main__':
+    # Testing with sample images
     imgL = cv2.imread('../data/tsukuba_l.png', 0)
     imgR = cv2.imread('../data/tsukuba_r.png', 0)
     disp = img_to_disp((imgL, imgR))
