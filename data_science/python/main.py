5ec6d5d4a8715efef3c711cb53d7fbe6b6030a89
Luke Parker <lukeaparker@gmail.com>
2020-02-16 16:40:45 -0800

    kevin is an r word

--- data_science/python/main.py
+++ data_science/python/main.py
@@ -2,7 +2,7 @@ from stream_object import Stream
 from img_to_disparity import img_to_disp as i2d
 from depth_to_haptic import depth_to_haptic
 import cProfile
-
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -12,11 +12,16 @@ cam1.start()
 def print_images():
     count = 0
     while count < 100:
+        plt.clf()
         tup = cam1.read()
         disp = i2d(tup)
-        haptic = depth_to_haptic(disp)
-        plt.imshow(haptic)
+        haptic = np.array(depth_to_haptic(disp))
+        plt.scatter(haptic[:, 0], haptic[:, 1])
+        plt.draw()
+        plt.pause(0.05)
         count += 1
 
     cam1.stop()
-cProfile.run('print_images()')
+
+plt.show()
+print_images()
